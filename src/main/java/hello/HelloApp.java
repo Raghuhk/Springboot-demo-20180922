8680f284a3009725dab6c78d415432268e0ac4e8
Bharadwaj <raghu.bharadwaj@accenture.com>
2018-09-22 16:50:50 +0800

    Initial commit

---
+++ src/main/java/hello/HelloApp.java
@@ -0,0 +1,12 @@
+package main.java.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApp {
+	
+	public static void main(String []s) {
+		SpringApplication.run(HelloApp.class, s);
+	}
+}
